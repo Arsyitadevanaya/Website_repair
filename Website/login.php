c68087c86c2cc569e007651120f013f16769ba50
Arsyitadevanaya <114077706+Arsyitadevanaya@users.noreply.github.com>
2023-08-08 11:20:27 +0700

    Update login.php

--- Website/login.php
+++ Website/login.php
@@ -1,15 +1,26 @@
 <?php
 require "function.php";
 
-if (isset($_POST['masuk'])) {
-    login($_POST);
-}
+
 
 if (isset($_SESSION["login_pelanggan"])) {
     header("Location: home.php");
     exit;
 }
+$loginResult = ""; // Initialize the variable
+$email_salah = "";
+$password_salah = "";
+if (isset($_POST['masuk'])) {
+    $loginResult = login($_POST);
+    // Use the $loginResult as needed
+    if ($loginResult === "Email salah") {
+        // Handle login failure
+        $email_salah = "Email Salah, masukkan kembali email yang benar.";
+    } elseif ($loginResult === "Password salah") {
+        $password_salah = "Password Salah, masukkan kembali password yang benar.";
 
+    }
+}
 if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
     $id = $_COOKIE['id'];
     $key = $_COOKIE['key'];
@@ -23,8 +34,8 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
         $_SESSION["login_pelanggan"] = true;
     }
 }
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="zxx">
 
@@ -36,7 +47,8 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Favicon -->
-    <link href="favicon_repair.png" rel="shortcut icon" />
+    <link href="img/favicon_repair.png" rel="shortcut icon" />
+
 
     <!-- Google font -->
     <link
@@ -54,85 +66,96 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
     <link rel="stylesheet" href="css/slicknav.min.css" />
 
     <!-- Main Stylesheets -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style1.css" />
+
+
+
 
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
     <style>
-        h1 {
-            color: white;
-            font-size: 60px;
-        }
-        label, p {
-            color: #cacaca;
+        .card-body.login-failed {
+            background-color: #f8d7da;
+            padding: 10px;
+            border: 1px solid #f5c6cb;
+            border-radius: 5px;
             text-align: center;
         }
 
-        .login-section .container-form,
-        .registrasi-section .container-form {
+        .card-body.login-failed h6 {
+            color: #721c24;
+            font-size: 18px;
+            font-weight: bold;
+            margin: 0;
+        }
+
+        .card-body.login-failed p {
+            color: #721c24;
+            font-size: 10px;
+            font-weight: bold;
+            margin: 0;
+        }
+        .wa_btn {
+            position: fixed;
+            right: 30px;
+            bottom: 50px;
+            text-decoration: none;
+            color: black;
+            background-color: rgb(35, 243, 16);
+            padding: 10px 20px;
+            border-radius: 13px;
             display: flex;
             justify-content: center;
             align-items: center;
-
-            /* Mengatur tinggi container agar mengisi tinggi viewport */
+            /* Tambahkan z-index dengan nilai tinggi */
+            z-index: 9999;
         }
 
-        .login-section p,
-        .registrasi-section p {
-            margin-bottom: 20px;
-            padding-top: 10px;
-        }
-
-        .login-section .card,
-        .registrasi-section .card {
-            background: #ffffff;
-            padding-top: 20px;
+        .wa_btn i {
+            font-size: 60px;
+            padding: 0 10px;
+            background-color: rgb(35, 243, 16);
+            border-radius: 50%;
+            height: 80px;
+            width: 80px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            right: 150px;
             color: white;
-            width: 70%;
-            /* Menggunakan lebar 100% agar kartu memenuhi lebar container */
-            max-width: 500px;
-            /* Mengatur lebar maksimum kartu */
-            background: #212121;
-            height: 100%;
-            box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.25);
         }
 
-        .login-section .card input[type="submit"],
-        .registrasi-section .card input[type="submit"] {
-            border-radius: 100px;
-            background: #ff2525;
-            border-color: #fc0254;
-            width: 200px;
-            height: 50px;
-            flex-shrink: 0;
-            color: #FFF;
-            font-size: 20px;
+        .wa_btn span {
+            color: white;
         }
 
-        .login-section .card input[type="submit"]:hover,
-        .login-section .card input[type="button"]:hover,
-        .registrasi-section .card input[type="submit"]:hover,
-        .registrasi-section .card input[type="button"]:hover {
-            color: black;
-            background-color: rgb(192, 192, 192);
-            border: none;
-        }
+        @media (max-width: 576px) {
+            .wa_btn {
+                background-color: transparent;
+            }
 
-        .login-section label, .registrasi-section label {
-            color: #868686;
-            color: #cacaca;
+            .wa_btn span {
+                display: none;
+            }
+
+            .wa_btn i {
+                right: 0;
+            }
+
+
         }
     </style>
 </head>
 
 <body>
     <!-- Page Preloder -->
-    <!-- <div id="preloder">
+    <div id="preloder">
         <div class="loader"></div>
-    </div> -->
-    
+    </div>
     <!-- Header section -->
     <header class="header-section clearfix">
         <a href="index.html" class="site-logo">
@@ -142,10 +165,10 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
         <div class="header-right">
             <div class="user-panel">
                 <?php if (!isset($_SESSION["login_pelanggan"]) || $_SESSION["login_pelanggan"] !== true) { ?>
-                    <a href="login.php" class="hr-btn">Login</a><span>|</span>
+                    <a href="login.php" class="login">Login</a><span>|</span>
                     <a href="registrasi.php" class="register">Create an account</a>
                 <?php } else { ?>
-                    <a href="profil.php" class="hr-btn">Profil</a><span>|</span>
+                    <a href="profil.php" class="login">Profil</a><span>|</span>
                     <a href="keluar.php" class="register">Keluar</a>
                 <?php } ?>
             </div>
@@ -160,24 +183,46 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
                     <li><a href="daftar_harga.php">Daftar Harga</a></li>
                 </ul>
             </li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#footer">Contact</a></li>
         </ul>
 
     </header>
 
     <!-- Header section end -->
-    <section class="registrasi-section spad">
+    <section class="login-section spad">
         <div class="container container-form">
-            <div class="card"><br>
+            <div class="card">
+
                 <h1 class="card-title" align="center">Login</h1>
-                <p><em>Silahkan masukkan informasi akun anda.</em></p>
+                <?php
+                if ($email_salah || $password_salah) { 
+                ?>
+                <div class="card-body login-failed">
+                    <?php if (!empty($email_salah)) { ?>
+                        <h6 align="center">Login Gagal!</h6>
+                        <p>
+                            <?php echo $email_salah; ?>
+                        </p>
+                    <?php } elseif (!empty($password_salah)) { ?>
+                        <h6 align="center">Login Gagal!</h6>
+                        <p>
+                            <?php echo $password_salah; ?>
+                        </p>
+                    <?php } ?>
+                </div>
+                <?php }?>
+
                 <div class="card-body">
                     <form action="#" method="post">
                         <input type="text" class="form-control" id="merekInput" placeholder="Email" autocomplete="off"
-                            name="email" required>
+                            name="email">
                         <br>
-                        <input type="password" class="form-control" id="merekInput" placeholder="Password"
-                            autocomplete="off" name="password" required><br>
+                        <div class="wrapper">
+                            <input type="password" name="password" class="form-control" placeholder="Enter Password"
+                                required>
+                            <span class="show-btn"><i class="fa fa-eye-slash"></i></span>
+                        </div>
+                        <p style="text-align: right;"><a href="lupa_password.php">Forgot Password?</a></p>
                         <div class="mb-3 form-check">
                             <input type="checkbox" class="form-check-input" id="exampleCheck1" name="cekbox">
                             <label class="form-check-label" for="exampleCheck1">Remember Me</label>
@@ -192,15 +237,19 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
         </div>
 
     </section>
+    <!-- Whatsapp section  -->
+    <a class="wa_btn" href="https://wa.me/6285175002568"><i class="fa fa-whatsapp fa-2xl"></i> <span> Chat
+            WhatsApp</span> </a>
+    <!-- Whatsapp section end -->
     <!-- Footer section -->
-    <footer class="footer-section">
+    <footer id="footer" class="footer-section">
         <div class="container">
             <div class="row">
-                <div class="col-xl-9 col-lg-8 order-lg-1">
+            <div class="col-xl-6 col-lg-5 order-lg-1">
                     <img src="img/logo_repair.png" alt="">
                 </div>
-                <div class="col-xl-3 col-lg-4 order-lg-2">
-                    <div class="footer-widget"><br>
+                <div class="col-xl-6 col-lg-7 order-lg-2">
+                    <div class="footer-widget">
                         <h2>Contact Us</h2>
                         <ul class="contact-list">
                             <li><a href="https://wa.me/6285175002568" target="_blank"><i class="fa fa-whatsapp"
@@ -211,8 +260,11 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
                                         style="color: #007BFF;"></i></a> repairlectric@gmail.com</li>
                         </ul>
                     </div>
+
                 </div>
-            </div><br>
+                
+            </div>
+
         </div>
         <div class="copyright">
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
@@ -229,5 +281,21 @@ if (isset($_COOKIE['id']) && isset($_COOKIE['key'])) {
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/mixitup.min.js"></script>
     <script src="js/main.js"></script>
+    <script>
+        const passField = document.querySelector("input[type='password']");
+        const showBtn = document.querySelector("span.show-btn");
+
+        showBtn.addEventListener("click", function () {
+            if (passField.type === "password") {
+                passField.type = "text";
+                showBtn.innerHTML = "<i class='fa fa-eye'></i>";
+            } else {
+                passField.type = "password";
+                showBtn.innerHTML = "<i class='fa fa-eye-slash'></i>";
+            }
+        });
+    </script>
+
 </body>
+
 </html>
