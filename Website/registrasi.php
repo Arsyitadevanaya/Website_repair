c170ebfab873280e4d375b264042e56802019076
Arsyitadevanaya <114077706+Arsyitadevanaya@users.noreply.github.com>
2023-08-08 12:55:58 +0700

    Update registrasi.php

--- Website/registrasi.php
+++ Website/registrasi.php
@@ -34,18 +34,58 @@ if (isset($_POST['registrasi'])) {
     <link rel="stylesheet" href="css/slicknav.min.css" />
 
     <!-- Main Stylesheets -->
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style1.css" />
+
 
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
     <style>
-        h1 {
+        .card h1 {
             color: white;
             font-size: 60px;
         }
-        p {
+
+        @media (max-width: 768px) {
+            .card h1 {
+                font-size: 30px;
+            }
+
+            .login-section .card input[type="submit"],
+            .registrasi-section .card input[type="submit"] {
+                border-radius: 0px;
+                background: #ff2525;
+                border-color: #fc0254;
+                width: 100px;
+                height: 25px;
+                flex-shrink: 0;
+                color: #FFF;
+                font-size: 5px;
+            }
+
+
+
+        }
+
+        @media (min-width: 769px) and (max-width: 992px) {
+            .card h1 {
+                font-size: 45px;
+            }
+
+
+        }
+
+        @media (min-width: 993px) {
+            .card h1 {
+                font-size: 60px;
+            }
+
+
+
+        }
+
+        .card p {
             color: #cacaca;
             text-align: center;
         }
@@ -100,9 +140,90 @@ if (isset($_POST['registrasi'])) {
             border: none;
         }
 
-        .login-section label, .registrasi-section label {
+        .login-section label,
+        .registrasi-section label {
             color: #868686;
             color: #cacaca;
+        }
+
+        h6 {
+            color: white;
+        }
+
+        .wa_btn {
+            position: fixed;
+            right: 30px;
+            bottom: 50px;
+            text-decoration: none;
+            color: black;
+            background-color: rgb(35, 243, 16);
+            padding: 10px 20px;
+            border-radius: 13px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            /* Tambahkan z-index dengan nilai tinggi */
+            z-index: 9999;
+        }
+
+        .wa_btn i {
+            font-size: 60px;
+            padding: 0 10px;
+            background-color: rgb(35, 243, 16);
+            border-radius: 50%;
+            height: 80px;
+            width: 80px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: absolute;
+            right: 150px;
+            color: white;
+        }
+
+        .wa_btn span {
+            color: white;
+        }
+
+        @media (max-width: 576px) {
+            .wa_btn {
+                background-color: transparent;
+            }
+
+            .wa_btn span {
+                display: none;
+            }
+
+            .wa_btn i {
+                right: 0;
+            }
+
+            .card form input {
+                font-size: 10px;
+                height: 25px;
+                margin-bottom: 5px;
+            }
+            .card form p{
+                font-size: 10px;
+            }
+
+            .login-section .card input[type="submit"],
+            .registrasi-section .card input[type="submit"] {
+                border-radius: 100px;
+                background: #ff2525;
+                border-color: #fc0254;
+                width: 199px;
+                height: 40px;
+                flex-shrink: 0;
+                color: #FFF;
+                font-size: 15px;
+            }
+
+        }
+
+        .card form input {
+
+            margin-bottom: 20px;
         }
     </style>
 </head>
@@ -122,10 +243,10 @@ if (isset($_POST['registrasi'])) {
         <div class="header-right">
             <div class="user-panel">
                 <?php if (!isset($_SESSION["login_pelanggan"]) || $_SESSION["login_pelanggan"] !== true) { ?>
-                    <a href="login.php" class="hr-btn">Login</a><span>|</span>
+                    <a href="login.php" class="login">Login</a><span>|</span>
                     <a href="registrasi.php" class="register">Create an account</a>
                 <?php } else { ?>
-                    <a href="profil.php" class="hr-btn">Profil</a><span>|</span>
+                    <a href="profil.php" class="login">Profil</a><span>|</span>
                     <a href="keluar.php" class="register">Keluar</a>
                 <?php } ?>
             </div>
@@ -140,44 +261,47 @@ if (isset($_POST['registrasi'])) {
                     <li><a href="daftar_harga.php">Daftar Harga</a></li>
                 </ul>
             </li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#footer">Contact</a></li>
         </ul>
 
     </header>
 
     <!-- Header section end -->
-    <section class="login-section spad">
+    <section class="registrasi-section spad">
         <div class="container container-form">
-            <div class="card"><br>
+            <div class="card">
                 <h1 class="card-title" align="center">Registrasi</h1>
-                <p><em>Silahkan isi form berikut untuk melanjutkan.</em></p>
                 <div class="card-body">
                     <form action="#" method="post">
                         <input type="text" class="form-control" id="merekInput" placeholder="Email" autocomplete="off"
-                            name="email" required><br>
+                            name="email">
                         <input type="password" class="form-control" id="jamInput" placeholder="Password"
-                            autocomplete="off" name="password" required><br>
+                            autocomplete="off" name="password">
                         <input type="password" class="form-control" id="jamInput" placeholder="Verifikasi Password"
-                            autocomplete="off" name="password2" required><br>
+                            autocomplete="off" name="password2">
                         <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                            <input type="submit" value="Registrasi" name="registrasi"><br><br>
-                        </div><br>
+                            <input type="submit" value="Registrasi" name="registrasi">
+                        </div>
                         <p align="center">Sudah Memiliki Akun? <a href="login.php">Login</a></p>
+
                     </form>
                 </div>
             </div>
         </div>
     </section>
-
+    <!-- Whatsapp section  -->
+    <a class="wa_btn" href="https://wa.me/6285175002568"><i class="fa fa-whatsapp fa-2xl"></i> <span> Chat
+            WhatsApp</span> </a>
+    <!-- Whatsapp section end -->
     <!-- Footer section -->
-	<footer class="footer-section">
-		<div class="container">
-			<div class="row">
-                <div class="col-xl-9 col-lg-8 order-lg-1">
+    <footer id="" class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-6 col-lg-5 order-lg-1">
                     <img src="img/logo_repair.png" alt="">
                 </div>
-                <div class="col-xl-3 col-lg-4 order-lg-2">
-                    <div class="footer-widget"><br>
+                <div class="col-xl-6 col-lg-7 order-lg-2">
+                    <div class="footer-widget">
                         <h2>Contact Us</h2>
                         <ul class="contact-list">
                             <li><a href="https://wa.me/6285175002568" target="_blank"><i class="fa fa-whatsapp"
@@ -188,17 +312,20 @@ if (isset($_POST['registrasi'])) {
                                         style="color: #007BFF;"></i></a> repairlectric@gmail.com</li>
                         </ul>
                     </div>
+
                 </div>
-            </div><br>
-		</div>
-		<div class="copyright">
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-			Copyright &copy;
-			<script>document.write(new Date().getFullYear());</script> by RepairLectric</a>
-			<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-		</div>
-	</footer>
-	<!-- Footer section end -->
+
+            </div>
+
+        </div>
+        <div class="copyright">
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear());</script> by RepairLectric</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+        </div>
+    </footer>
+    <!-- Footer section end -->
 
     <!--====== Javascripts & Jquery ======-->
     <script src="js/jquery-3.2.1.min.js"></script>
@@ -207,5 +334,7 @@ if (isset($_POST['registrasi'])) {
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/mixitup.min.js"></script>
     <script src="js/main.js"></script>
+
 </body>
+
 </html>
